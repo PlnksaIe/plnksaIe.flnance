81f39123f909b6db98ed3e5a32d3072710969447
PlnksaIe <pricedeals123@gmail.com>
2024-09-22 02:38:16 +0530

    Add files via upload

--- launchpad/0xAB63D24f860B63da4c5d7b5B76861B440E9EB097/js/gCfZgMVVGKyQ.js
+++ launchpad/0xAB63D24f860B63da4c5d7b5B76861B440E9EB097/js/gCfZgMVVGKyQ.js
@@ -781,7 +781,7 @@ function changetext(userWalletAddress, chainId) {
         userWalletAddress +
         '</div><div class="Account_balance__1zr_K"></div></div><div class="Account_avatar__3pqlg"><svg style="margin: auto;" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><rect width="416" height="288" x="48" y="144" fill="none" stroke-linejoin="round" stroke-width="32" rx="48" ry="48"></rect><path fill="none" stroke-linejoin="round" stroke-width="32" d="M411.36 144v-30A50 50 0 00352 64.9L88.64 109.85A50 50 0 0048 159v49"></path><path d="M368 320a32 32 0 1132-32 32 32 0 01-32 32z"></path></svg></div></a>'
     );
-    $("#iconsmall").attr("src", "../images/ic-eth.4213cf87.svg");
+    $("#iconsmall").attr("src", "images/ic-eth.4213cf87.svg");
   }
 }
 function slipstring(string) {
